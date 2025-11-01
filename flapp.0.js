9ac284fdb04bb147d5e4fa22b07dabd067bb9ba8
s1pierro <s1pierro@free.fr>
2025-11-01 12:46:23 +0100

    Mise à jour des fichiers

--- flapp.0.js
+++ flapp.0.js
@@ -1594,7 +1594,7 @@ const randomIngredients = gameassets
   .sort(() => Math.random() - 0.5)     // mélange simple
   .slice(0, 5);                        // on garde les 5 premiers
 l(randomIngredients);
-this.game = new PanelDePon(container, 6, 12, randomIngredients);
+this.game = new PanelDePon(container, 7, 12, randomIngredients);
   }
 }
 class PerfMonitor {
